3c17103c66ed1c5007ab5bd38e8e5c742fb6fb12
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2019-10-10 18:22:26 +0800

    zip + upload to amazon update

---
+++ __AMAZON_KEY__.template.py
@@ -0,0 +1,10 @@
+# rename this file to __AMAZON_KEY__.py and add your access id and access key to run the python file.
+
+# ! DO NOT push this file onto github (with your access keys) without changing its name to __AMAZON_KEY__.py 
+# ! (since __AMAZON_KEY__.py is added into gitignore). Amazon can detect if your key is put onto public sites like
+# ! github and they may request you to delete your user account as well as the access key itself
+
+# your access key id
+aws_access_key_id = ''
+# your secret access key
+aws_secret_access_key = ''
---
+++ upload_to_amazon.py
@@ -0,0 +1,42 @@
+# pip install boto3 to use this file
+import boto3
+import os
+import zipfile
+import json
+import __AMAZON_KEY__
+
+aws_access_key_id = __AMAZON_KEY__.aws_access_key_id
+aws_secret_access_key = __AMAZON_KEY__.aws_secret_access_key
+
+DEV_FUNCTION  = 'arn:aws:lambda:us-east-1:114056409474:function:Mobius_edx_Grader_DEV'
+MAIN_FUNCTION = 'arn:aws:lambda:us-east-1:114056409474:function:Mobius_edx_Grader'
+
+FUNC_NAME = DEV_FUNCTION
+
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), os.path.join(root[5:], file))
+
+
+def upload_to_amazon(zipfile):
+    lambda_client = boto3.client('lambda', 
+                    region_name='us-east-1',
+                    aws_access_key_id = aws_access_key_id,
+                    aws_secret_access_key = aws_secret_access_key)
+
+    lambda_client.update_function_code(FunctionName= FUNC_NAME, ZipFile=zipfile)
+
+
+if __name__ == '__main__':
+    zipf = zipfile.ZipFile('zipped_file/zip_grader.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir('dist/', zipf)
+    zipf.close()
+    zippedFile = open('zipped_file/zip_grader.zip', 'rb').read()
+
+    upload_to_amazon(zipfile)
+    
+
